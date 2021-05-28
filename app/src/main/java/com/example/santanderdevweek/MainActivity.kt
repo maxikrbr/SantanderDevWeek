e118191dd5d66ef9261f57e29be8041695c43009
Carlos Maximiliano <maxi.krbr@gmail.com>
2021-05-27 23:01:47 -0300

    git remote add origin https://github.com/maxikrbr/SantanderDevWeek.git git branch -M main git push -u origin main

--- app/src/main/java/com/example/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/example/santanderdevweek/MainActivity.kt
@@ -2,15 +2,35 @@ package com.example.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.item_1 -> {
+                Log.d("CLICK", "Item 1 active")
+                true
+            }
+            R.id.item_2 -> {
+                Log.d("CLICK", "Item 2 active")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
